15e3e069dea2890b42ba2e21e2718d3366e34894
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-03-06 09:33:49 +0530

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -16,3 +16,20 @@ console.log('Stopping..');
 // Stopping..
 // 0 sec Timer
 // 2 sec Timer
+
+// Call Stack ( main >> setTimeout(..., 2000) >> setTimeout(..., 0) >> console.log('Fin...')
+//                   >> Callback (0 sec) >> console.log('Zero sec...') >> Callback (2 sec) >> console.log('Two...'))
+//              Node APIs ( setTimeout (2 sec) >> setTimeout (0 sec) )
+//                          Event Loop / Callback Queue ( Callback (0 sec) >> Callback (2 sec) )
+
+// JavaScript is SingleThreaded programming language
+// Non blocking nature of Code
+
+// Callback Queue -maintain a list of the callback functions that are ready to get executed
+// Event Loop - Looks at the call stack  and  call back queue,
+//                 if call stack is empty, its going to run items from the call back queue
+
+// HTTP Requests
+// https://darksky.net/forecast/40.727    --taken over by apple
+// https://weatherstack.com  >> SignUp for Free tier >> API Key
+// BASE URL --> https://api.weatherstack.com

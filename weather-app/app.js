bc2b3c45b4edfa0a7e9cca88907ea730b2f453e7
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-03-08 09:06:19 +0530

    Customizing HTTP Requests
    Customizing HTTP Requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -29,7 +29,7 @@
 // Event Loop - Looks at the call stack  and  call back queue,
 //                 if call stack is empty, its going to run items from the call back queue
 
-// Making HTTP Requests
+// Making HTTP Requests                               ---------------------------(*)
 // https://darksky.net/forecast/40.727    --taken over by apple
 // https://weatherstack.com  >> SignUp for Free tier >> API Key
 // BASE URL --> https://api.weatherstack.com
@@ -52,4 +52,123 @@
 //   console.log(data.current);
 // });
 
-// Customizing HTTP Requests
+// Customizing HTTP Requests                           ---------------------------(*)
+
+// const request = require('request');
+
+// const url =
+//   'https://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,122.4233';
+
+// request({ url: url, json: true }, (error, response) => {
+//   console.log(response.body.current);
+// });
+
+// To work with JSON, we can add "JSON Formatter" extension on Chrome
+
+//
+// Goal: Print a small forecast to the user            ---------------------------(*)
+//
+// 1. Print: "It is currently 9 degress out. It flles like 5 degrees out."
+// 2. Test your work!
+
+// const request = require('request');
+
+// const url =
+//   'https://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,122.4233';
+
+// request({ url: url, json: true }, (error, response) => {
+//   //   console.log(response.body);
+//   //   console.log(response.body.current);
+//   console.log(
+//     'It is currently ' +
+//       response.body.current.temperature +
+//       ' degrees out. It feels like ' +
+//       response.body.current.feelslike +
+//       ' degrees out'
+//   );
+// });
+
+// // To change the units (like *C to *F)  (&units=f)  ---------------------------(*)
+
+// const request = require('request');
+
+// const url =
+//   'https://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,122.4233&units=f';
+
+// request({ url: url, json: true }, (error, response) => {
+//   //   console.log(response.body);
+//   //   console.log(response.body.current);
+//   console.log(
+//     'It is currently ' +
+//       response.body.current.temperature +
+//       ' degrees out. It feels like ' +
+//       response.body.current.feelslike +
+//       ' degrees out'
+//   );
+// });
+
+// // To add WeatherDescription                        ---------------------------(*)
+
+// const request = require('request');
+
+// const url =
+//   'https://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,122.4233&units=f';
+
+// request({ url: url, json: true }, (error, response) => {
+//   //   console.log(response.body);
+//   //   console.log(response.body.current);
+//   console.log(
+//     response.body.current.weather_descriptions[0] +
+//       '. It is currently ' +
+//       response.body.current.temperature +
+//       ' degrees out. It feels like ' +
+//       response.body.current.feelslike +
+//       ' degrees out'
+//   );
+// });
+
+// // HTTP request Challenge                          ---------------------------(*)
+// Geocoding
+// Address -> Lat/Long -> Weather
+// mapbox.com
+// https://docs.mapbox.com/api/search
+// https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json
+// https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json?access_token=pk.eyJ1joI.....&limit=1
+
+// {
+//     "type": "FC",
+//     "query": {},
+//     "features": [],
+//     "attribution": ""
+// }
+
+// features :- place_name >> LA, US
+//             center >> longitude, latitude
+
+//
+// Goal: Print the lat/long for Los Angeles    -------------(*)
+//
+// 1. Fire off a new request to the URL explored in browser
+// 2. Have the request module parse it as JSON
+// 3. Print both the latitude and longitude to the terminal
+// 4. Test your work!
+
+// const request = require('request');
+
+// const url =
+//   'https://api.weatherstack.com/current?access_key=8cf78b463a4dccfca6ef49cda44bf3a0&query=37.8267,122.4233&units=f';
+// const urlmp =
+//   'https://api.mapbox.com/geocoding/v5/{endpoint}/{search_text}.json?access_token=pk.eyJ1joI.....&limit=1';
+// // ('https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1joI.....&limit=1');
+
+// request({ url: urlmp, json: true }, (error, response) => {
+//   //   console.log(response.body);
+//   //   console.log(response.body.current);
+//   const latitude = response.body.features[0].center[1];
+//   const longitude = response.body.features[0].center[0];
+//   //   console.log('Latitude : ' + latitude + '\n' + 'Longitude : ' + longitude);
+//   console.log(latitude, longitude);
+// });
+
+// // Handling Errors                                ---------------------------(*)
+// Suppose if we are not connected to Internet

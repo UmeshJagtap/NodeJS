1ca12b4ae6a75857f915398ae7954e9a4d55d8ae
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-03-13 19:34:14 +0530

    template for help page

--- web-server/NOTES.js
+++ web-server/NOTES.js
@@ -138,3 +138,49 @@
   </body>
 </html> */
 }
+
+// React
+// https://github.com/academind/react-complete-guide-course-resources/tree/main/attachments
+
+// // // Customizing the views directory    -----------------------------(*)
+
+// const path = require('path');
+// const express = require('express');
+
+// const app = express();
+// const publicDirectoryPath = path.join(__dirname, '../public');
+
+// app.set('view engine', 'hbs');
+// app.use(express.static(publicDirectoryPath));
+
+// app.get('', (req, res) => {
+//   res.render('index', {
+//     title: 'Weather App',
+//     name: 'Andrew Mead',
+//   });
+// });
+
+// app.get('/about', (req, res) => {
+//   res.render('about', {
+//     title: 'About Me',
+//     name: 'Andrew',
+//   });
+// });
+
+// app.get('/help', (req, res) => {
+//   res.render('help', {
+//     example: 'Help Me',
+//     helpText: 'This is some helpful text.',
+//   });
+// });
+
+// app.get('/weather', (req, res) => {
+//   res.send({
+//     forecast: 'It is snowing',
+//     location: 'Pune',
+//   });
+// });
+
+// app.listen(3000, () => {
+//   console.log('Server is up on port 3000.');
+// });
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -144,57 +144,55 @@
 // Have added css file as web-server >> public >> css >> styles.css   and updated the link tag
 // And Image as web-server >> public >> img >> robo.jpg   and updated the img tag
 
-// // Dynamic Pages with Templating
+// // // Dynamic Pages with Templating
 
-// npm handlebars  --templating tool (low level lib)
-// npm hbs
+// // npm handlebars  --templating tool (low level lib)
+// // npm hbs
 
-const path = require('path');
-const express = require('express');
+// const path = require('path');
+// const express = require('express');
 
-const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
+// const app = express();
+// const publicDirectoryPath = path.join(__dirname, '../public');
 
-app.set('view engine', 'hbs');
-app.use(express.static(publicDirectoryPath));
+// app.set('view engine', 'hbs');
+// app.use(express.static(publicDirectoryPath));
 
-app.get('', (req, res) => {
-  res.render('index', {
-    title: 'Weather App',
-    name: 'Andrew Mead',
-  });
-});
+// app.get('', (req, res) => {
+//   res.render('index', {
+//     title: 'Weather App',
+//     name: 'Andrew Mead',
+//   });
+// });
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    title: 'About Me',
-    name: 'Andrew',
-  });
-});
+// app.get('/about', (req, res) => {
+//   res.render('about', {
+//     title: 'About Me',
+//     name: 'Andrew',
+//   });
+// });
 
-//
-// Goal: Create a template for help page
-//
-// 1. Setup a help template to render a help message to the screen
-// 2. Setup the help route and render the template with an example message
-// 3. Visit the route in browser and see your help message print
+// //
+// // Goal: Create a template for help page
+// //
+// // 1. Setup a help template to render a help message to the screen
+// // 2. Setup the help route and render the template with an example message
+// // 3. Visit the route in browser and see your help message print
 
-app.get('/help', (req, res) => {
-  res.render('help', {
-    example: 'Help Me',
-    helpText: 'This is some helpful text.',
-  });
-});
+// app.get('/help', (req, res) => {
+//   res.render('help', {
+//     example: 'Help Me',
+//     helpText: 'This is some helpful text.',
+//   });
+// });
 
-app.get('/weather', (req, res) => {
-  res.send({
-    forecast: 'It is snowing',
-    location: 'Pune',
-  });
-});
+// app.get('/weather', (req, res) => {
+//   res.send({
+//     forecast: 'It is snowing',
+//     location: 'Pune',
+//   });
+// });
 
-app.listen(3000, () => {
-  console.log('Server is up on port 3000.');
-});
-
-// // Customizing the views directory    -----------------------------(*)
+// app.listen(3000, () => {
+//   console.log('Server is up on port 3000.');
+// });

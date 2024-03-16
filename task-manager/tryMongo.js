f0212e8bea2e1d5843ffe3fd68f6656f5af446f9
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2024-03-16 22:17:50 +0530

    tryMongo
    tryMongo

---
+++ task-manager/tryMongo.js
@@ -0,0 +1,23 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      console.log('Unable to connect to database!');
+    }
+
+    // console.log('Connected correctly');
+
+    const db = client.db(databaseName); // creates a new db connection to specific database
+    db.collection('users').insertOne({
+      name: 'Andrew',
+      age: 27,
+    });
+  }
+);

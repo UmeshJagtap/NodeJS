e3d67ed07b8bf9ed98559b800f8e99b73966db6f
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-03-01 23:43:55 +0530

    streams_n_pipes
    streams_n_pipes

---
+++ abc/node_fp_e1/streams_n_pipes/notes.js
@@ -0,0 +1,26 @@
+// Streams
+
+// Types of streams
+// There are four types of streams:
+
+// Readable - For read operation
+// Writable - For write operation
+// Duplex - For both read and write operation
+// Transform - When the output is computed based on the input
+
+// Piping in Node
+// Piping is a mechanism to connect output of one stream to another stream.
+// It is normally used to get data from one stream and to pass the output of that stream to another stream.
+// There is no limit on piping operations.
+
+// Pipes in streams
+
+// Chaining in Node
+// Chaining is a mechanism to connect output of one stream to another stream and create a chain of multiple stream operations.
+// It is normally used with piping operations.
+
+// Streams - Reading Files -------------------------------- HANDS_ON
+
+// Streams - Write to File -------------------------------- HANDS_ON
+
+// Streams - File Copy ------------------------------------ HANDS_ON

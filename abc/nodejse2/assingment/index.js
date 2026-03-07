86d1d17dbcf0be18961fb3b2fb07293ac6b74606
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-03-07 10:50:08 +0530

    Create user
    Create user

--- abc/nodejse2/assingment/index.js
+++ abc/nodejse2/assingment/index.js
@@ -4,7 +4,6 @@
 // Password
 
 import express from 'express';
-import ejs from 'ejs';
 
 const app = express();
 
@@ -43,6 +42,58 @@ app.get('/users', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send('Hello world');
+});
+
+// Create task
+// app.post('/user', (req, res) => {
+//   const [title, status = 'pending'] = req.body || {};
+//   if (!title || typeof title !== 'string') {
+//     return res
+//       .status(400)
+//       .json({ error: 'BadRequest', messge: 'Title is required (string)' });
+//   }
+//   if (!['pending', 'in-progress', 'done'].includes(status)) {
+//     return res
+//       .status(400)
+//       .json({ error: 'BadRequest', message: 'Invalid status' });
+//   }
+//   const task = {
+//     id: nextId++,
+//     title,
+//     status,
+//     createdAt: new Date().toDateString(),
+//   };
+//   tasks.push(task);
+//   res.status(201).json(task);
+// });
+
+// Create user
+app.post('/user', (req, res) => {
+  const { name, email, password, image } = req.body || {};
+  if (!name || typeof name !== 'string') {
+    return res
+      .status(400)
+      .json({ error: 'BadRequest', message: 'Name is required (string)' });
+  }
+  if (!email || typeof email !== 'string') {
+    return res
+      .status(400)
+      .json({ error: 'BadRequest', message: 'Email is required' });
+  }
+  if (!password || typeof password !== 'string') {
+    return res
+      .status(400)
+      .json({ error: 'BadRequest', message: 'Password is required' });
+  }
+  const user = {
+    id: nextId++,
+    name,
+    email,
+    password,
+    image,
+  };
+  users.push(user);
+  res.status(201).json(user);
 });
 
 app.listen(3000, () => {

6cf25758459d090c9ab17507277c95f0a2d4968a
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-03-17 09:36:42 +0530

    assignment
    assignment

---
+++ abc/nodejse2/assingment/routes/empRoutes.js
@@ -0,0 +1,141 @@
+import express from 'express';
+
+// const bcrypt = require('bcrypt.js');
+import bcrypt from 'bcryptjs';
+
+// const jwt = require('jsonwebtoken');
+import jwt from 'jsonwebtoken';
+
+// const db = require("../config/database");
+// const verifyToken = require('../middleware/auth');
+import verifyToken from '../middleware/auth.js';
+
+const router = express.Router();
+
+const users = [
+  {
+    id: 1,
+    empname: 'Rohan',
+    email: 'Rohan@gmail.com',
+    password: 'Rohan123',
+  },
+  {
+    id: 2,
+    empname: 'Rohan2',
+    email: 'Rohan2@gmail.com',
+    password: 'Rohan123',
+  },
+];
+
+router.post('/create', async (req, res) => {
+  const { empname, email, password } = req.body;
+
+  const hashpwd = await bcrypt.hash(password, 10);
+
+  const new_user = {
+    empname: empname,
+    email: email,
+    password: hashpwd,
+  };
+  users.push(new_user);
+  res.json({ message: 'emp created' });
+  console.log('users : ', users);
+
+  // const query = 'insert into emps (name, email, password) values (?, ?, ?)';
+
+  // db.query(query, [empname, email, hashpwd)], (err, result) => {
+  // 	if(err) return res.status(500).json(err);
+  // 	res.json({ message: "emp created"});
+  // });
+});
+
+router.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  console.log('email : ', email);
+  console.log('password : ', password);
+
+  const user_logging_in = users.find((user) => user.email === email);
+  if (!user_logging_in)
+    return res.status(400).json({ message: 'emp not found' });
+
+  // const usersWithRoleUser = users.filter(user => user.role === "User");
+
+  const match = bcrypt.compare(password, user_logging_in.password);
+
+  if (!match) {
+    return res.status(400).json({ message: 'invalid credentials' });
+  }
+  const token = jwt.sign(
+    { id: user_logging_in.id, email: user_logging_in.email },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: '30',
+    }
+  );
+
+  res.json({ token });
+
+  // db.query("select * from emps where email = ?", [email], async (err, result) => {
+  //  	if(err) return res.status(500).json(err);
+
+  // 	if (result.length === 0) {
+  // 		return res.status(400).json({ message: "emp not found"})
+  // 	}
+  // 	const emp = result[0];
+
+  // 	const match = await bcrypt.compare(password, emp.password);
+
+  // 	if (!match) {
+  // 		return res.status(400).json({message: 'invalid credentials'})
+  //   }
+  // 	const token = jwt.sign(
+  // 		{id: emp.id, email: emp},
+  // 		process.env.JWT_SECRET,
+  // 		{expiresIn: "30"}
+  // 	);
+  // 	// jwt Token >> asass.aada.xxsax  aka  header.payload.signature
+
+  // 	res.json({token})
+  // })
+});
+
+// Authorization: Bearer TOKEN
+
+router.get('/profile', verifyToken, (req, res) => {
+  res.set('Content-Type', 'text/plain'); // Set a single header
+  // res.set({
+  //   'Cache-Control': 'no-cache', // Set multiple headers using an object
+  //   'X-Custom-Header': 'my-value',
+  // });
+  res.send('protected data');
+  //
+  res.json({ message: 'protected data', emp: req.emp });
+});
+
+// module.exports = router;
+export default router;
+
+// OUTPUT
+// POST >> http://localhost:3000/api/emps/login
+//
+// Body >> raw >> JSON
+// {
+//   "email": "Rohan@gmail.com",
+//   "password": "Rohan123"
+// }
+//
+// {"token":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJSb2hhbkBnbWFpbC5jb20iLCJpYXQiOjE3NzI5OTE5ODcsImV4cCI6MTc3Mjk5MTk4N30.v3L-BjrD59heeOEWd_v7Xze-DYPDi-ww1-P9xIJLsiA"}
+//
+// POST >> http://localhost:3000/api/emps/create
+// Body >> raw >> JSON
+// {
+//   "empname":"Umesh",
+//   "email":"Umesh123@mail.com",
+//   "password":"Umesh123"
+// }
+//
+// {
+//   "message": "emp created"
+// }
+//
+// Issue :- JSON not getting updated

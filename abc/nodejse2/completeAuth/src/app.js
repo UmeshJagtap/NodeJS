5ef62f68d8e82270d87fcfc629b6fd9cd49729f6
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-04-23 16:49:41 +0530

    added cookie-parser
    added cookie-parser

--- abc/nodejse2/completeAuth/src/app.js
+++ abc/nodejse2/completeAuth/src/app.js
@@ -2,13 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import authRouter from './routes/auth.route.js';
 // import jwt from 'jsonwebtoken';
-// import cookieparser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
-// app.use(cookieparser());
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 
 // app.use(express.urlencoded({ extended: true }));
 // const JWT_SECRET = 'your-jwt-secret-key';
